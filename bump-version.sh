230779cef46ee6182e5185bd022b92d7266ae38d
Raphael Nestler <raphael.nestler@gmail.com>
2023-10-09 14:12:28 +0200

    Add script to bump versions automatically

---
+++ bump-version.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+
+set -euo pipefail
+
+function die() {
+    echo "$@"
+    exit 1
+}
+
+
+[ $# -eq 1 ] || die "Usage: $0 [new_version]"
+
+VERSION_REGEX="1\.[0-9]+\.[0-9]+"
+new_version="$1"
+
+[[ "$new_version" =~ $VERSION_REGEX ]] || die "Invalid version $new_version"
+
+git switch "bump-versions-to-include-$1"
+
+sed -i -E "s/($VERSION_REGEX) ($VERSION_REGEX) ($VERSION_REGEX)/\\2 \\3 $new_version/g" .github/workflows/publish-docker-images.yml
+
+git add .github/workflows/publish-docker-images.yml
+
+git commit -m "Bump versions to include $new_version"
